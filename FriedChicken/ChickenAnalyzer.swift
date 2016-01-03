57acac7776bf6c4ae4d006cf3e685cb4e3ab5626
oga <oga.ivc.s27@gmail.com>
2016-01-03 22:49:32 +0900

    create a passing image func to chicken analyzer.

---
+++ FriedChicken/ChickenAnalyzer.swift
@@ -0,0 +1,23 @@
+//
+//  ChickenAnalyzer.swift
+//  FriedChicken
+//
+//  Created by JunyaOgasawara on 2016/01/03.
+//  Copyright © 2016年 Japan Karaage Association. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChickenAnalyzer {
+    let img :CGImage
+
+    init(image: CGImage) {
+        self.img = image
+    }
+
+    func analyze() -> (Int) {
+        let score =  0
+        return score
+    }
+}
--- FriedChicken/HomeViewController.swift
+++ FriedChicken/HomeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController ,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,38 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /**
+     ライブラリから写真を選択する
+     */
+    func pickImageFromLibrary() {
+
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+
+            let controller = UIImagePickerController()
+            controller.delegate = self
+
+            controller.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+
+            //新たに追加したカメラロール表示ViewControllerをpresentViewControllerにする
+            self.presentViewController(controller, animated: true, completion: nil)
+        }
+    }
+
+    // 写真選択後に呼ばれるCallback
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo: [String: AnyObject]) {
+        if didFinishPickingMediaWithInfo[UIImagePickerControllerOriginalImage] != nil {
+
+            let img = didFinishPickingMediaWithInfo[UIImagePickerControllerOriginalImage] as? UIImage
+            let score = ChickenAnalyzer(image: img!.CGImage!).analyze()
+            NSLog("%d", score)
+        }
+        //写真選択後にカメラロール表示ViewControllerを引っ込める動作
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func onClickSelectImageBtn(sender: AnyObject) {
+        pickImageFromLibrary()
+    }
 
 }
 
