61add04bf1a1b891242aa8e931a4f8e21c0edd5c
oga <oga.ivc.s27@gmail.com>
2016-01-23 01:19:48 +0900

    remove redundant data object.

--- FriedChicken/AnalysisHistoryViewController.swift
+++ FriedChicken/AnalysisHistoryViewController.swift
@@ -14,7 +14,7 @@ class AnalysisHistoryViewController: UIViewController, UITableViewDataSource, UI
 
     @IBOutlet weak var tableView: UITableView!
 
-    var listEntries: Array<DataHolder> = []
+    var analysisResults: Array<ChickenAnalysisResult> = []
     var selectedResult: ChickenAnalysisResult?
 
     override func viewDidLoad() {
@@ -23,60 +23,36 @@ class AnalysisHistoryViewController: UIViewController, UITableViewDataSource, UI
         tableView.delegate = self
         tableView.dataSource = self
 
-        listEntries = getInitialEntries()
+        analysisResults = getInitialEntries()
     }
 
-    func getInitialEntries() -> Array<DataHolder> {
+    func getInitialEntries() -> Array<ChickenAnalysisResult> {
         let realm = try! Realm()
         return realm.objects(ChickenAnalysisResult)
             .sorted("createdAt", ascending: false)
-            .map { result in
-                return DataHolder(objectId: result.objectId,
-                    img: result.img(),
-                    score: result.score,
-                    date: result.createdAt)
-        }
+            .map { $0 }
     }
 
     // セルの行数
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return min(listEntries.count, MAX_ROW_COUNT_OF_ENTRIES)
+        return min(analysisResults.count, MAX_ROW_COUNT_OF_ENTRIES)
     }
 
     // セルのテキストを追加
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell  = tableView.dequeueReusableCellWithIdentifier("analysis_result_table_view_cell",
             forIndexPath: indexPath) as! AnalysisResultTableViewCell
-        let holder = listEntries[indexPath.row]
+        let result = analysisResults[indexPath.row]
 
-        cell.titleLabel.text = "\(String(holder.score))点の唐揚げ"
-        cell.imgView.image = holder.img
-        cell.dateLabel.text = dateToString(holder.date)
+        cell.titleLabel.text = "\(String(result.score))点の唐揚げ"
+        cell.imgView.image = result.img()
+        cell.dateLabel.text = dateToString(result.createdAt)
         return cell
     }
 
     func tableView(table: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath) {
-
-        let realm = try! Realm()
-        if let result = realm.objectForPrimaryKey(ChickenAnalysisResult.self,
-            key: listEntries[indexPath.row].objectId) {
-            selectedResult = result
-            self.performSegueWithIdentifier("show_history_result", sender: self)
-        }
-    }
-
-    class DataHolder {
-        let objectId: String
-        let img: UIImage
-        let score: Int
-        let date: NSDate
-
-        init(objectId: String, img: UIImage, score: Int, date: NSDate) {
-            self.objectId = objectId
-            self.img = img
-            self.score = score
-            self.date = date
-        }
+        selectedResult = analysisResults[indexPath.row]
+        self.performSegueWithIdentifier("show_history_result", sender: self)
     }
 
     func dateToString(date: NSDate) -> String {
